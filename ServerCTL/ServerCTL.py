8e86698716061197762160ff2d92234bf6c66ea2
Renato S Dias <39096504+RSilvaDias@users.noreply.github.com>
2021-10-16 14:38:27 -0300

    Update and rename ServerCTL.py to ServerCTL/ServerCTL.py

--- ServerCTL/ServerCTL.py
+++ ServerCTL/ServerCTL.py
@@ -42,18 +42,24 @@ def create_service(Deployname):
     )
     #Creation of the deployment on namespace default
     core_v1_api.create_namespaced_service(namespace="default", body=body)
+    print("Service created ! Name : %s-service" % Deployname)
+
+def getClusterIP(Deployname):
     config.load_kube_config()
+    core_v1_api = client.CoreV1Api()
     service = core_v1_api.read_namespaced_service(name="%s-service" % Deployname, namespace="default")
     print("Cluster_IP :" ,service.spec.cluster_ip)
+    return service.spec.cluster_ip
 
 def VerifyStatus(pod_name):
-       
+    config.load_kube_config()
     core_v1 = core_v1_api.CoreV1Api()
     api_response = core_v1.read_namespaced_pod(name=pod_name,namespace="default")
-    return api_response.status.phase 
-    
+    print("Pod Status : " , api_response.status.phase)
+    return api_response.status.phase
+
 def createDeployment(name):
-    
+
     config.load_kube_config()
     api_instance = core_v1_api.CoreV1Api()
     with open(path.join(path.dirname(__file__),'%s.yaml' %name)) as f:
@@ -63,14 +69,21 @@ def createDeployment(name):
                 body=dep,namespace="default")
             print("Deployment created. Deployment Name = '%s'" % resp.metadata.name)
     os.remove('%s.yaml' %name)
-    create_service(name)
+    #create_service(name)
 
 def main():
 
-    name = DeploymentName()
-    createDeployment(name)
-    print("Pod_Name : ", PodName(name))
-    print("Pod Status :", VerifyStatus(PodName(name)))
-    
+    name = DeploymentName()                                          # Create yaml file with given name
+    print("1 - Create deployment \n2 - Create Service \n3 - Verify status of POD \n4 - Show Cluster IP\n5 - exit\n")
+    while True:
+        var = input("Input : ").strip()
+        if ( var == "1" ):  createDeployment(name)                  # Create a deployment given a name
+        elif (var == "2" ): create_service(name)                    # Create a Service and expose to type ClusterIP
+        #PodName(name)                                              # Given the Deploy name , returns the Pod name
+        elif (var == "3" ): VerifyStatus(PodName(name))             # Requires the name of the Pod to verify
+        elif (var == "4" ): getClusterIP(name)                      # Return the Cluster_IP of the exposed service
+        elif (var == "5" ): exit()
+        else : print("Invalid input")
+
 if __name__ == '__main__':
     main()
