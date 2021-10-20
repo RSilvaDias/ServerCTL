97996611360643eb0cf8e9307082e766ad6e7c7a
Renato S Dias <39096504+RSilvaDias@users.noreply.github.com>
2021-10-19 22:00:46 -0300

    Add files via upload

---
+++ ServerCTL/POD/Client/clientctl.py
@@ -0,0 +1,18 @@
+
+import zmq
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to CTL server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://clt:5555")  # CTL pod ClusterIP
+
+print("Name of deploy : ")
+name = input()
+print("Sending request…")
+socket.send(name.encode())
+
+
+message = socket.recv()
+print(f"Received reply [ {message} ]")
---
+++ ServerCTL/POD/Client/loop.py
@@ -0,0 +1,5 @@
+import time
+
+while True:
+    print("Working")
+    time.sleep(10)
